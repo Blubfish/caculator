1201e98cc37a538305fbbc0ee18c89455a64c547
moncheeta <moncheeta@duck.com>
2023-09-20 10:55:06 -0500

    Update UI

--- Converter/Converter.swift
+++ Converter/Converter.swift
@@ -7,12 +7,67 @@
 
 import SwiftUI
 
+enum From {
+    case Decimal
+}
+
+enum To {
+    case Binary
+}
+
 @main
 struct Converter: App {
+    @State var from: From = .Decimal
+    @State var to: To = .Binary
+    @State var input: String = ""
+    @State var output: String = ""
+    
     var body: some Scene {
         WindowGroup {
-            DecimalToBinary()
+            VStack {
+                HStack {
+                    Menu("From") {
+                        Button("Decimal", action: {
+                            self.from = .Decimal
+                        })
+                    }
+                    Spacer()
+                    Menu("To") {
+                        Button("Binary", action: {
+                            self.to = .Binary
+                        })
+                    }
+                }
+                HStack {
+                    TextField({ () -> String in
+                        switch self.from {
+                        case .Decimal:
+                            return "decimal"
+                        }
+                    }(), text: self.$input)
+                    .onSubmit {
+                        switch self.from {
+                        case .Decimal:
+                            switch self.to {
+                            case .Binary:
+                                self.output = DecimalToBinary(input: self.input)
+                            }
+                        }
+                    }
+                    TextField({ () -> String in
+                        if output.isEmpty {
+                            switch self.to {
+                            case .Binary:
+                                return "binary"
+                            }
+                        }
+                        return self.output
+                    }(), text: self.$output)
+                    .multilineTextAlignment(.trailing)
+                    .disabled(true)
+                }
+            }
+            .padding()
         }
     }
-    
 }
--- Converter/DecimalToBinary.swift
+++ Converter/DecimalToBinary.swift
@@ -7,35 +7,25 @@
 
 import SwiftUI
 
-struct DecimalToBinary: View {
-    @State var input: String = ""
-    @State var binary: [Int] = []
-    
-    var body: some View {
-        TextField("decimal", text: self.$input)
-            .onSubmit {
-                self.binary = []
-                var decimal = { () -> Int in
-                    if let decimal = Int(self.input) {
-                        return decimal
-                    } else {
-                        return 0
-                    }
-                }()
-                var place = 0
-                while decimal > 0 {
-                    let bit = decimal % 2
-                    self.binary.append(bit)
-                    place += 1
-                    decimal = decimal / 2
-                }
-            }
-        Text({ () -> String in
-            var text = ""
-            for bit in binary.reversed() {
-                text += String(bit)
-            }
-            return text
-        }())
+func DecimalToBinary(input: String) -> String {
+    var binary: [Int] = []
+    var decimal = { () -> Int in
+        if let decimal = Int(input) {
+            return decimal
+        } else {
+            return 0
+        }
+    }()
+    var place = 0
+    while decimal > 0 {
+        let bit = decimal % 2
+        binary.append(bit)
+        place += 1
+        decimal = decimal / 2
     }
+    var text = ""
+    for bit in binary.reversed() {
+        text += String(bit)
+    }
+    return text
 }
